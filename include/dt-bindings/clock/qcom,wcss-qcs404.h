2adbca241c2244a34a0c0e79ca67788f87bf3203
Govind Singh <govinds@codeaurora.org>
2018-08-17 11:47:24 +0530

    FROMLIST: dt-bindings: clock: qcom: Introduce QCOM WCSS Q6DSP clock bindings
    Add device tree bindings for WiFi QDSP subsystem clock controls
    found in OCS404 soc.
    
    (am from https://patchwork.kernel.org/patch/10794355/)
    
    BUG=b:128831966
    TEST=None
    
    Change-Id: I778888effd77b11c4b498d2b5d3a598b5a8a96ab
    Cc: linux-clk@vger.kernel.org, linux-arm-msm@vger.kernel.org
    Signed-off-by: Govind Singh <govinds@codeaurora.org>
    Reviewed-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Rakesh Pillai <pillair@codeaurora.org>
    Signed-off-by: Vikas Patel <vikpatel@codeaurora.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1531631
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Zhihong Yu <zhihongyu@chromium.org>
    Commit-Queue: Zhihong Yu <zhihongyu@chromium.org>
    Tested-by: Zhihong Yu <zhihongyu@chromium.org>

---
+++ include/dt-bindings/clock/qcom,wcss-qcs404.h
@@ -0,0 +1,25 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2018, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef _DT_BINDINGS_CLK_WCSS_QCS404_H
+#define _DT_BINDINGS_CLK_WCSS_QCS404_H
+
+#define WCSS_AHBFABRIC_CBCR_CLK			0
+#define WCSS_AHBS_CBCR_CLK			1
+#define WCSS_TCM_CBCR_CLK			2
+#define WCSS_AHBM_CBCR_CLK			3
+#define WCSS_AXIM_CBCR_CLK			4
+#define WCSS_BCR_CBCR_CLK			5
+#define WCSS_LCC_CBCR_CLK			6
+#define WCSS_QDSP6SS_XO_CBCR_CLK		7
+#define WCSS_QDSP6SS_SLEEP_CBCR_CLK		8
+#define WCSS_QDSP6SS_GFMMUX_CLK			9
+
+#define Q6SSTOP_QDSP6SS_RESET			0
+#define Q6SSTOP_QDSP6SS_CORE_RESET		1
+#define Q6SSTOP_QDSP6SS_BUS_RESET		2
+#define Q6SSTOP_BCR_RESET			3
+#define Q6SSTOP_CORE_ARCH_RESET			4
+#endif
