fe0b77b00457ee9c59e68cc070216e65d5a50d01
xiaofeis <xiaofeis@codeaurora.org>
2019-03-21 12:17:11 +0800

    CHROMIUM: WAR: net: dsa: Implement flow_dissect callback for tag_qca
    Add flow_dissect for qca tagged packet to get the right hash.
    
    Change-Id: I660ef3427fb98aaa7896d19972eea011e7aab778
    Signed-off-by: xiaofeis <xiaofeis@codeaurora.org>
    Signed-off-by: Sumit Agrawal <agsumit@codeaurora.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1535720
    Reviewed-by: Zhihong Yu <zhihongyu@chromium.org>
    Commit-Queue: Zhihong Yu <zhihongyu@chromium.org>
    Tested-by: Zhihong Yu <zhihongyu@chromium.org>

--- net/dsa/tag_qca.c
+++ net/dsa/tag_qca.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2015, 2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -113,8 +113,18 @@ static struct sk_buff *qca_tag_rcv(struct sk_buff *skb, struct net_device *dev,
 	return skb;
 }
 
+static int qca_tag_flow_dissect(const struct sk_buff *skb, __be16 *proto,
+				int *offset)
+{
+	*offset = QCA_HDR_LEN;
+	*proto = ((__be16 *)skb->data)[0];
+
+	return 0;
+}
+
 const struct dsa_device_ops qca_netdev_ops = {
 	.xmit	= qca_tag_xmit,
 	.rcv	= qca_tag_rcv,
+	.flow_dissect = qca_tag_flow_dissect,
 	.overhead = QCA_HDR_LEN,
 };
